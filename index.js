701e7d7de34f3a58bc2879954bb7db7aa91e4823
Christine <christinekhekama99@gmail.com>
2023-06-14 17:37:56 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+const cards= ["Guadalupe", "Ollie", " Aki"];
+
+function writeCards(cards){
+    const array=[];
+    for (let i=0; i<cards.length;i++){
+        const messages=`Thank you, ${cards[i]}, for the wonderful surprise gift!`;
+        array.push(messages) }
+        return array; 
+    
+}
+writeCards(cards);
+console.log(writeCards(cards));
+
+function countDown(){
+    let i=10; while (i>=0){
+        console.log(i);i--
+    }
+    return i;
+}
+countDown();
